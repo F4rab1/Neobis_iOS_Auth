c042197087d6913a315d4edbbcb72315e3285451
F4rab1 <issafarabi2022@gmail.com>
2023-05-30 01:39:34 +0600

    Initial Commit

---
+++ Neobis_iOS_Auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Auth
+//
+//  Created by Фараби Иса on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
