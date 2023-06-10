ab92eb0979d9912ec0c110a841a49964066f5d69
F4rab1 <issafarabi2022@gmail.com>
2023-06-10 18:29:06 +0600

    commit

--- Neobis_iOS_Auth/Service/RegistrationService.swift
+++ Neobis_iOS_Auth/Service/RegistrationService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class RegistrationService {
-    func register(email: String, completion: @escaping (Result<String, Error>) -> Void) {
+    func register(email: String, completion: @escaping (Result<Data, Error>) -> Void) {
         let baseURL = URL(string: "http://34.107.1.158/auth/")!
         let registerURL = baseURL.appendingPathComponent("register-for-mobile")
         var request = URLRequest(url: registerURL)
@@ -30,43 +30,10 @@ class RegistrationService {
             if let error = error {
                 completion(.failure(error))
                 return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
-                return
-            }
-            
-            guard let data = data else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
-                return
-            }
-            
-            if httpResponse.statusCode == 200 {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    
-                    if let jsonObject = json as? [String: Any],
-                       let email = jsonObject["email"] as? String,
-                       let message = jsonObject["message"] as? String {
-                        completion(.success("Verification message has been sent to your email: \(email). \(message)"))
-                    } else if let jsonArray = json as? [[String: Any]] {
-                        if let errorMessage = jsonArray.first?["email"] as? String {
-                            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage])))
-                        } else {
-                            completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
-                        }
-                    } else {
-                        completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
-                    }
-                } catch {
-                    completion(.failure(error))
-                }
-            } else {
-                completion(.failure(NSError(domain: "", code: httpResponse.statusCode, userInfo: nil)))
+            } else if let data = data {
+                completion(.success(data))
             }
         }
-        
         task.resume()
     }
 }
